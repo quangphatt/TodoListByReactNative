c287633ad5b355722c872c9a856367e90b8d8ea7
Quang Phat <phatquang2811@gmail.com>
2022-01-23 08:15:34 +0700

    design Task component

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import Task from './components/Task';
 
 const App = () => {
 	return (
 		<View style={styles.container}>
-			<Text>Hello World</Text>
+
+			<View style={styles.tasksWrapper}>
+				<Text style={styles.sectionTitle}>Today's Task</Text>
+				<View style={styles.items}>
+					<Task text="Hello"/>
+					<Task text="Hello every body"/>
+				</View>
+			</View>
+			
 		</View>
 	);
 };
 
 const styles=StyleSheet.create({
 	container: {
-		flex:1,
-		alignItems:'center',
-		justifyContent: 'center'
-	}
+		flex: 1,
+    	backgroundColor: '#E8EAED',
+	},
+	tasksWrapper: {
+		paddingTop: 80,
+		paddingHorizontal: 20,
+	},
+	sectionTitle: {
+		fontSize: 24,
+		fontWeight: 'bold'
+	},
+	items: {
+		marginTop: 30,
+	},
+	writeTaskWrapper: {
+		position: 'absolute',
+		bottom: 60,
+		width: '100%',
+		flexDirection: 'row',
+		justifyContent: 'space-around',
+		alignItems: 'center'
+	},
+	input: {
+		paddingVertical: 15,
+		paddingHorizontal: 15,
+		backgroundColor: '#FFF',
+		borderRadius: 60,
+		borderColor: '#C0C0C0',
+		borderWidth: 1,
+		width: 250,
+	},
+	addWrapper: {
+		width: 60,
+		height: 60,
+		backgroundColor: '#FFF',
+		borderRadius: 60,
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderColor: '#C0C0C0',
+		borderWidth: 1,
+	},
+	addText: {},
 });
 
 export default App;
